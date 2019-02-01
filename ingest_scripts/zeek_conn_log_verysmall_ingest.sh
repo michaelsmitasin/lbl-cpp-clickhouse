50c21915f8249502174cbb47138e2311749e2922
Michael Smitasin <46496263+michaelsmitasin@users.noreply.github.com>
2019-01-31 17:06:27 -0800

    Change to file date not timestamp interpreted date
    Timestamp date is based on first packet seen, while file date is based on last packet seen. More useful to know file date to pivot to search on that file.

--- ingest_scripts/zeek_conn_log_verysmall_ingest.sh
+++ ingest_scripts/zeek_conn_log_verysmall_ingest.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
-# This combines orig_h and resp_h to a single column, ip, for faster indexing
 # Example usage:
 # zeek_conn_log_verysmall_ingest.sh zeek_conn_log_verysmall.data | clickhouse-client --query="INSERT INTO zeek_conn_log_verysmall FORMAT TabSeparated"
+
+FILEDATE=$(echo $1 | cut -d"." -f4 | cut -d"-" -f-3)
 
 # excluse comments / headers
 zfgrep -v "#" $1 | \
 # convert time format
-gawk '{print strftime("%Y-%m-%d", $1)"\t"$3"\n"strftime("%Y-%m-%d", $1)"\t"$5}' 
+gawk -v DATEVAR="$FILEDATE" '{print DATEVAR"\t"$3"\n"DATEVAR"\t"$5}' 
 
 exit
