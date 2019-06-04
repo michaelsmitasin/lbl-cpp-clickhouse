8077ecd36681ae90b1d3fdfc5ddbc5bdcf616692
Michael Smitasin <46496263+michaelsmitasin@users.noreply.github.com>
2019-06-04 16:47:56 -0700

    Create zeek_conn_log_index_ingest.sh

---
+++ ingest_scripts/zeek_conn_log_index_ingest.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+# Example usage:
+# zeek_conn_log_verysmall_ingest.sh zeek_conn_log_verysmall.data | clickhouse-client --query="INSERT INTO zeek_conn_log_verysmall FORMAT TabSeparated"
+
+FILEDATE=$(echo $1 | cut -d"." -f4 | cut -d"-" -f-3)
+
+# excluse comments / headers
+zfgrep -v "#" $1 | \
+        # convert time format
+gawk -v DATEVAR="$FILEDATE" '{print DATEVAR"\t"$3"\t"$2"\n"DATEVAR"\t"$5"\t"$2}' 
+
+exit
